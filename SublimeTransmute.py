6b4cc17fd124151500b4924ca446a6f716e4a1e3
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-23 18:36:33 -0400

    Copied in code snippet to restructure and understand sublime API funcition calls

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -1,7 +1,14 @@
 import sublime, sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
-        sublime.status_message("TEST STATUS MESSAGE")
+  def on_done(self, index):
+    self.view.run_command("insert_my_text", {"args":{'text':self.list[index]}})
+
+  def run(self, edit):
+    self.list = ["one", "two", "buckle my", "shoe"]
+    self.view.window().show_quick_panel(self.list, self.on_done)
+
+class InsertMyText(sublime_plugin.TextCommand):
+  def run(self, edit, args):
+    self.view.insert(edit, 0, args['text'])
 
