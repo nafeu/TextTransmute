537027986b8b1d125b8e8c1940ffe6d70334389b
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-28 15:48:06 -0400

    reset plugin and added key-binds to start

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -1,14 +1,8 @@
-import sublime, sublime_plugin
+
+import sublime
+import sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
-  def on_done(self, index):
-    self.view.run_command("insert_my_text", {"args":{'text':self.list[index]}})
-
   def run(self, edit):
-    self.list = ["one", "two", "buckle my", "shoe"]
-    self.view.window().show_quick_panel(self.list, self.on_done)
-
-class InsertMyText(sublime_plugin.TextCommand):
-  def run(self, edit, args):
-    self.view.insert(edit, 0, args['text'])
-
+    allcontent = sublime.Region(0, self.view.size())
+    self.view.replace(edit, allcontent, 'Hello, world!')
