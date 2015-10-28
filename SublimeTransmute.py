10132cf5476aa44bb309660f7c9c2343a67cbe43
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-28 17:24:12 -0400

    changed functionality to accept user input with show_input_panel and insert it onto active window

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -1,8 +1,11 @@
-
 import sublime
 import sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    allcontent = sublime.Region(0, self.view.size())
-    self.view.replace(edit, allcontent, 'Hello, world!')
+    def run(self, edit):
+
+        def on_done(text):
+            print(text);
+            self.view.run_command("insert", {"characters": text})
+        # self.view.insert(edit, self.view.size(), 'Hello, world!')
+        sublime.active_window().show_input_panel("caption", "initial_text", on_done, None, None)
