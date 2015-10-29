7a8b51644a9d5ea9ec7e6e92bdae379270af58f3
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-29 13:15:46 -0400

    learned how to use commands as helper functions

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -6,17 +6,41 @@ class ExampleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
         def on_done(text):
-            self.view.run_command("insert", {"characters": text})
+            # self.view.run_command("insert", {"characters": text})
+            if (text == 'length'):
+                # print("length works")
+                for region in self.view.sel():
+                    length = str(len(self.view.substr(region)))
+                    region_begin = region.begin()
+                    region_end = region.end()
+                    # self.view.run_command("replace", {"region": region, "string": "asdf", "asdf": "asdf"})
+                    # self.view.run_command("insert", {"characters": length})
+                    self.view.run_command("transmute", {"region_begin": region_begin, "region_end": region_end, "string": length})
+            else:
+                sublime.error_message("invalid command")
 
-        selection = self.view.sel()
-        output = ''
-
-        for region in self.view.sel():
-            length = str(len(self.view.substr(region)))
-            self.view.replace(edit, region, length)
-            # output += '\n' + self.view.substr(region)
+        sublime.active_window().show_input_panel("caption", "length", on_done, None, None)
 
         # self.view.replace(edit, self.view.size(), output)
 
-        # sublime.active_window().show_input_panel("caption", "initial_text", on_done, None, None)
+class ParseCommand(sublime_plugin.TextCommand):
+    def run(self, edit, user_input):
+        command_list = user_input.split("|")
+
+
+class TransmuteCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, region_begin, region_end, string):
+        region = sublime.Region(region_begin, region_end)
+        self.view.replace(edit, region, string)
+
+'''
+
+1. Input
+2. Parse
+3. Map to Command
+4. Process Transmutation
+5. Replace/Insert Text
+
+'''
 
