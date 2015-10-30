063392e3b3ccd5b0ebf41b88faeb07e0c71c52cc
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-30 14:50:09 -0400

    Added clipboard insertion functionality (into the command pipe)

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -10,38 +10,27 @@ class ExampleCommand(sublime_plugin.TextCommand):
         sublime.active_window().show_input_panel("Transmute Code", "length", on_done, None, None)
 
 class ParseCommand(sublime_plugin.TextCommand):
-    '''
-    Parse Logic:
-    '''
+
     def run(self, edit, user_input):
+        error_status = False
         region_set = self.view.sel()
         # command_list = user_input.split("|")
         command_list = [x.strip() for x in user_input.split('|')]
-        # sublime.error_message(str(command_list))
         m = MutationEngine()
-
         for region in region_set:
             # grab the content of the region
             body = self.view.substr(region)
             for command in command_list:
                 # go through each command and mutate it accordingly
-                body = m.mutate(body, command)
+                try:
+                    body = m.mutate(body, command)
+                except InvalidTransmutation as e:
+                    sublime.error_message("Invalid Transmutation Command: \n\n'" + e.value + "'")
+                    error_status = True
+                    break
             # call the transmutation to replace it on your screen
-            print("OUTPUT BODY IS: " + body)
-            self.view.run_command("transmute", {"region_begin" : region.begin(), "region_end" : region.end(), "string" : body})
-
-    # def mutator(self, body, command_name):
-    #     output = ""
-    #     if (command_name == "length"):
-    #         output = len(body)
-    #     elif (command_name == "repeat"):
-    #         output = body + body
-    #     elif (command_name == "reverse"):
-    #         output = body[::-1]
-    #     else:
-    #         print("Invalid transmutation command: '" + command_name + "'")
-    #         return body
-    #     return str(output)
+            if not error_status:
+                self.view.run_command("transmute", {"region_begin" : region.begin(), "region_end" : region.end(), "string" : body})
 
 class MutationEngine:
 
@@ -63,17 +52,34 @@ class MutationEngine:
         if command in self.command_lib:
             return str(eval("self."+command)())
         else:
-            print("Invalid Command: " + command)
-            return body
+            raise InvalidTransmutation(command)
+
+    # Mutation Methods --- ADD NEW METHODS HERE
+
+    def clip(self):
+        return sublime.get_clipboard()
 
     def length(self):
         return len(self.body)
 
+    def repeat(self):
+        return self.body + self.body
+
+
+    def reverse(self):
+        return self.body[::-1]
+
+
 class TransmuteCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, region_begin, region_end, string):
-        region = sublime.Region(region_begin, region_end)
-        self.view.replace(edit, region, string)
+        self.view.replace(edit, sublime.Region(region_begin, region_end), string)
+
+class InvalidTransmutation(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 '''
 
