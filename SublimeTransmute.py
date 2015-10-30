4bd9f9f0ea88f68dc948a68b32ae70534058b471
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-30 14:10:44 -0400

    added MutationEngine class to replace mutate function

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -4,61 +4,70 @@ import sublime_plugin
 class ExampleCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-
         def on_done(text):
             self.view.run_command("parse", {"user_input": text})
-            # if (text == 'length'):
-            #     for region in self.view.sel():
-            #         length = str(len(self.view.substr(region)))
-            #         region_begin = region.begin()
-            #         region_end = region.end()
-            #         self.view.run_command("transmute", {"region_begin": region_begin, "region_end": region_end, "string": length})
-            # else:
-            #     sublime.error_message("invalid command")
 
-        sublime.active_window().show_input_panel("Transmute Code", "", on_done, None, None)
+        sublime.active_window().show_input_panel("Transmute Code", "length", on_done, None, None)
 
 class ParseCommand(sublime_plugin.TextCommand):
     '''
-
     Parse Logic:
-
-
     '''
-
     def run(self, edit, user_input):
         region_set = self.view.sel()
         # command_list = user_input.split("|")
         command_list = [x.strip() for x in user_input.split('|')]
         # sublime.error_message(str(command_list))
+        m = MutationEngine()
 
         for region in region_set:
             # grab the content of the region
             body = self.view.substr(region)
             for command in command_list:
                 # go through each command and mutate it accordingly
-                body = self.mutator(body, command)
+                body = m.mutate(body, command)
             # call the transmutation to replace it on your screen
+            print("OUTPUT BODY IS: " + body)
             self.view.run_command("transmute", {"region_begin" : region.begin(), "region_end" : region.end(), "string" : body})
 
-    def mutator(self, body, command_name):
-        output = ""
-        if (command_name == "length"):
-            output = len(body)
-        elif (command_name == "repeat"):
-            output = body + body
-        elif (command_name == "reverse"):
-            output = body[::-1]
+    # def mutator(self, body, command_name):
+    #     output = ""
+    #     if (command_name == "length"):
+    #         output = len(body)
+    #     elif (command_name == "repeat"):
+    #         output = body + body
+    #     elif (command_name == "reverse"):
+    #         output = body[::-1]
+    #     else:
+    #         print("Invalid transmutation command: '" + command_name + "'")
+    #         return body
+    #     return str(output)
+
+class MutationEngine:
+
+    def __init__(self):
+        self.body = ""
+        self.command_name = ""
+        self.args = ""
+        # list all the methods that are not default object methods
+        self.command_lib = [
+            method for method in dir(MutationEngine)
+                if callable(getattr(MutationEngine, method))
+                and ("__" not in method)
+                and ("mutate" not in method)
+        ]
+
+    def mutate(self, body, command):
+        self.body = body
+        # also later consider splitting up the arguments here
+        if command in self.command_lib:
+            return str(eval("self."+command)())
         else:
-            print("Invalid transmutation command: '" + command_name + "'")
+            print("Invalid Command: " + command)
             return body
-        return str(output)
 
-
-class MutateCommand(sublime_plugin.TextCommand):
-
-    def run(self, edit, command_name, region_content):
-        return True
+    def length(self):
+        return len(self.body)
 
 class TransmuteCommand(sublime_plugin.TextCommand):
 
