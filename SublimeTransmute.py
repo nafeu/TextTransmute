d3718cf02c3920ff7b2ac657d776567084f7e4e2
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-10-29 23:49:30 -0400

    added simple multi command pipe logic with example commands

--- SublimeTransmute.py
+++ SublimeTransmute.py
@@ -6,27 +6,59 @@ class ExampleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
         def on_done(text):
-            # self.view.run_command("insert", {"characters": text})
-            if (text == 'length'):
-                # print("length works")
-                for region in self.view.sel():
-                    length = str(len(self.view.substr(region)))
-                    region_begin = region.begin()
-                    region_end = region.end()
-                    # self.view.run_command("replace", {"region": region, "string": "asdf", "asdf": "asdf"})
-                    # self.view.run_command("insert", {"characters": length})
-                    self.view.run_command("transmute", {"region_begin": region_begin, "region_end": region_end, "string": length})
-            else:
-                sublime.error_message("invalid command")
+            self.view.run_command("parse", {"user_input": text})
+            # if (text == 'length'):
+            #     for region in self.view.sel():
+            #         length = str(len(self.view.substr(region)))
+            #         region_begin = region.begin()
+            #         region_end = region.end()
+            #         self.view.run_command("transmute", {"region_begin": region_begin, "region_end": region_end, "string": length})
+            # else:
+            #     sublime.error_message("invalid command")
 
-        sublime.active_window().show_input_panel("caption", "length", on_done, None, None)
-
-        # self.view.replace(edit, self.view.size(), output)
+        sublime.active_window().show_input_panel("Transmute Code", "", on_done, None, None)
 
 class ParseCommand(sublime_plugin.TextCommand):
-    def run(self, edit, user_input):
-        command_list = user_input.split("|")
+    '''
 
+    Parse Logic:
+
+
+    '''
+
+    def run(self, edit, user_input):
+        region_set = self.view.sel()
+        # command_list = user_input.split("|")
+        command_list = [x.strip() for x in user_input.split('|')]
+        # sublime.error_message(str(command_list))
+
+        for region in region_set:
+            # grab the content of the region
+            body = self.view.substr(region)
+            for command in command_list:
+                # go through each command and mutate it accordingly
+                body = self.mutator(body, command)
+            # call the transmutation to replace it on your screen
+            self.view.run_command("transmute", {"region_begin" : region.begin(), "region_end" : region.end(), "string" : body})
+
+    def mutator(self, body, command_name):
+        output = ""
+        if (command_name == "length"):
+            output = len(body)
+        elif (command_name == "repeat"):
+            output = body + body
+        elif (command_name == "reverse"):
+            output = body[::-1]
+        else:
+            print("Invalid transmutation command: '" + command_name + "'")
+            return body
+        return str(output)
+
+
+class MutateCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, command_name, region_content):
+        return True
 
 class TransmuteCommand(sublime_plugin.TextCommand):
 
