06fefb31ac3bf6bb97529be95d431fa311edb45c
Nafeu Nasir <nafeu.nasir@gmail.com>
2015-11-16 20:55:05 -0500

    made test cases less verbose and reflective of exact methods inside mutate command functions

--- tests/count_command_test.py
+++ tests/count_command_test.py
@@ -5,71 +5,31 @@ m = MutationEngine()
 
 class TestCountCommand(unittest.TestCase):
 
-    def setUp(self):
-        self.orig = ''
+    def test_default_char_count(self):
+        self.assertEqual(int(m.mutate('','count')), 0)
+        self.assertEqual(int(m.mutate('a','count')), 1)
+        self.assertEqual(int(m.mutate('a#1','count')), 3)
+        self.assertEqual(int(m.mutate(' ','count')), 1)
+        self.assertEqual(int(m.mutate('\n','count')), 1)
+        self.assertEqual(int(m.mutate('a b','count')), 3)
+        self.assertEqual(int(m.mutate('a\nb','count')), 3)
 
-    def test_null_input(self):
-        self.orig = ''
-        self.assertEqual(int(m.mutate(self.orig,'count')), 0)
-        self.assertEqual(int(m.mutate(self.orig,'count -l')), 1)
-        self.assertEqual(int(m.mutate(self.orig,'count -w')), 0)
-        self.assertEqual(int(m.mutate(self.orig,'count -s a')), 0)
+    def test_line_count(self):
+        self.assertEqual(int(m.mutate('','count -l')), 1)
+        self.assertEqual(int(m.mutate('\n','count -l')), 2)
+        self.assertEqual(int(m.mutate('\n\n','count -l')), 3)
 
-    def test_single_char(self):
-        self.orig = 'a'
-        self.assertEqual(int(m.mutate(self.orig,'count')), 1)
+    def test_word_count(self):
+        self.assertEqual(int(m.mutate('','count -w')), 0)
+        self.assertEqual(int(m.mutate('foo','count -w')), 1)
+        self.assertEqual(int(m.mutate('foo bar','count -w')), 2)
 
-    def test_multiple_char(self):
-        self.orig = 'a#1'
-        self.assertEqual(int(m.mutate(self.orig,'count')), 3)
-
-    def test_whitespace_char(self):
-        self.orig = ' '
-        self.assertEqual(int(m.mutate(self.orig,'count')), 1)
-
-    def test_newline_char(self):
-        self.orig = '\n'
-        self.assertEqual(int(m.mutate(self.orig,'count')), 1)
-
-    def test_mixed_whitespace_char(self):
-        self.orig = 'a b'
-        self.assertEqual(int(m.mutate(self.orig,'count')), 3)
-
-    def test_mixed_newline_char(self):
-        self.orig = 'a\nb'
-        self.assertEqual(int(m.mutate(self.orig,'count')), 3)
-
-    def test_single_line(self):
-        self.orig = '\n'
-        self.assertEqual(int(m.mutate(self.orig,'count -l')), 2)
-
-    def test_multiple_line(self):
-        self.orig = '\n\n'
-        self.assertEqual(int(m.mutate(self.orig,'count -l')), 3)
-
-    def test_single_word(self):
-        self.orig = 'foo'
-        self.assertEqual(int(m.mutate(self.orig,'count -w')), 1)
-
-    def test_multiple_word(self):
-        self.orig = 'foo bar'
-        self.assertEqual(int(m.mutate(self.orig,'count -w')), 2)
-
-    def test_char_substr(self):
-        self.orig = 'a'
-        self.assertEqual(int(m.mutate(self.orig,'count -s a')), 1)
-
-    def test_word_substr(self):
-        self.orig = 'foo'
-        self.assertEqual(int(m.mutate(self.orig,'count -s foo')), 1)
-
-    def test_multiple_char_substr(self):
-        self.orig = 'a aa'
-        self.assertEqual(int(m.mutate(self.orig,'count -s a')), 3)
-
-    def test_multiple_word_substr(self):
-        self.orig = 'foo foofoo'
-        self.assertEqual(int(m.mutate(self.orig,'count -s foo')), 3)
+    def test_string_count(self):
+        self.assertEqual(int(m.mutate('','count -s a')), 0)
+        self.assertEqual(int(m.mutate('a','count -s a')), 1)
+        self.assertEqual(int(m.mutate('foo','count -s foo')), 1)
+        self.assertEqual(int(m.mutate('a aa','count -s a')), 3)
+        self.assertEqual(int(m.mutate('foo foofoo','count -s foo')), 3)
 
 if __name__ == '__main__':
     unittest.main()
