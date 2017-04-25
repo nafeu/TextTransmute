b7187e66456d40e464cadec8fd679a92ccf3d6e0
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-04-25 15:37:24 -0400

    Update default aliases

--- alias.py
+++ alias.py
@@ -1,8 +1,14 @@
 ALIASES = [
 
+  ["Execute GET request to specified url and params", "http get"],
+  ["Execute POST request to specified url and params", "http post"],
   ["Evaluate this expression", "expr"],
-  ["Generate a list from 1 to 10", "gen -s {$}. 1 10"],
-  ["Generate a list from a to e", "gen -s {$}. a e"],
+  ["Insert empty lines between selected", "expand"],
+  ["Flatten lines into one line seperated by whitespace", "compress"],
+  ["Convert sequence to Python dictionary", "map py"],
+  ["Convert sequence to Javascript object", "map js"],
+  ["Generate a list from 1 to 10", "mklist -s {$}. 1 10"],
+  ["Generate a list from a to e", "mklist -s {$}. a e"],
   # ["Description", "Command sequence"],
 
 ]
